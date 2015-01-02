15fcd069910c3018b74585fb6b40afe8a59a1546
Ahmed <wuzzyview@gmail.com>
2015-01-02 02:30:33 -0500

    Init

---
+++ lt-promise.js
@@ -0,0 +1,17 @@
+function getOnline(url) {
+    return new Promise(function(fulfill, reject) {
+        d3.xhr(url, 'text/plain', function(err, req) {
+            if (err) {
+                reject(err);
+            } else {
+                fulfill(req.responseText);
+            }
+        });
+    })
+}
+
+function testAll(url1, url2){
+    return Promise.all([ getOnline(url1), getOnline(url2) ])
+        .then(function(multiple) { console.log(multiple); })};
+
+testAll('foo', 'bar');
